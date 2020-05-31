23f5bf6a3363ecf6bd92f8329ed1fb122c918a51
Kazhymurat <komirzhan@gmail.com>
2020-05-31 17:54:27 +0600

    added login and signup pages

--- main.go
+++ main.go
@@ -8,6 +8,12 @@ import (
 func main() {
 	fs := http.FileServer(http.Dir("./static"))
 	http.Handle("/", fs)
+	http.HandleFunc("/signup", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "static/signup.html")
+	})
+	http.HandleFunc("/login", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "static/login.html")
+	})
 
 	log.Println("Listening on :8000...")
 	err := http.ListenAndServe(":8000", nil)
